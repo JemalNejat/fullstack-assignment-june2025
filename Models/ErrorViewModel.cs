205ef76c5d68097614b8ea3c36de977982a9717b
JemalNejat <143702419+JemalNejat@users.noreply.github.com>
2025-05-20 14:05:20 +0200

    Delete Models directory

--- Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace CDN.ITEGAMAX._4._0.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
